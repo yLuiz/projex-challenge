1723da05cd4ede736aef17b1ec718f0f507f6f43
Luiz Victor  Nascimento Andrade <lvictor@digiboard.com.br>
2023-03-24 13:39:56 -0400

    Ajeitando configuranção de deploy

--- backend/index.ts
+++ backend/index.ts
@@ -22,7 +22,7 @@ app.use('/property', propertiesRouter);
 
 
 app.use(errors());
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`
     =====================================================
