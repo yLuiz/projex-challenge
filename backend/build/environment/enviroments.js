447aac04ffb20e57e633d505082b3a2fdc263596
Luiz Victor  Nascimento Andrade <lvictor@digiboard.com.br>
2023-03-24 13:43:33 -0400

    Build folder remove

--- backend/build/environment/enviroments.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.environments = void 0;
-exports.environments = {
-    JWT_SECRET: "0552ef305d9ee0225f06041adb8d641f"
-};
