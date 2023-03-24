020103aed15198a093c9d42bc39cf8cc29492d0a
Luiz Victor  Nascimento Andrade <lvictor@digiboard.com.br>
2023-03-24 15:05:10 -0400

    Adicionando dependencia

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -9,6 +9,8 @@ export const environment = {
   production: false
 };
 
+// new comment
+
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
